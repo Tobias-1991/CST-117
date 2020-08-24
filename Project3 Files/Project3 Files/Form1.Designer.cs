0180ace24f59b340bec28d1eb2a43d184ced3aa4
Tobias-1991 <62919937+Tobias-1991@users.noreply.github.com>
2020-08-23 23:17:20 -0700

    Project 3 Files

---
+++ Project3 Files/Project3 Files/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Project3_Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(80, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(437, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(323, 369);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.Location = new System.Drawing.Point(80, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 78);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save File";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Project3 Files/Project3 Files/Form1.cs
@@ -0,0 +1,177 @@
+﻿/*
+Zachary Gardner
+CST-117
+Professor Pritchard
+August 23, 2020d
+ */
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ *Write a windows form application that does the following:
+Uses a file dialog control to allow the user to select a file for input
+Processes the contents of the file as follows:
+Converts all the words to lower-case
+Finds the first and last word alphabetically
+Finds the longest word
+Finds the word with the most vowels
+Displays the above statistics in a textbox
+Writes the above statistics to a file 
+ */
+namespace Project3_Files
+{
+    
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            string[] words;
+            string longestWord = "";
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "txt files (*.txt)|*.txt|Word docs (*.docx)|*.docx|Rich Text (*.rtf)|*.rtf";
+
+            if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filePath = fileDialog.FileName;
+
+                var fileStream = fileDialog.OpenFile();
+
+                string[] delimiter = new string[] {" ", "\r\n", ",", ".", "(", ")"};
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+
+                    fileContent = reader.ReadToEnd();
+                }
+                //Converts all the words to lower-case
+                string fileString = fileContent.ToString().ToLower();
+
+                // Finds the first and last word alphabetically
+                words = fileString.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+
+                // Sorts words alphabetically
+                Array.Sort(words);
+
+                //Finds the longest word
+                for (int word = 0; word < words.Length; word++)
+                {
+                    if (words[word].ToString().Length > longestWord.Length)
+                    {
+                        longestWord = words[word];
+                    }
+                }
+
+                //Finds the word with the most vowels
+              
+               
+             /*   var mostVowels = 0;
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    var part = words[i];
+                    var numberOfVowels = 0;
+                    foreach (var vowel in vowels)
+                    {
+                        if (part.Contains(vowel)) numberOfVowels++;
+                    }
+
+                    if (mostVowels < numberOfVowels)
+                    {
+                        mostVowels = i;
+                        word = part;
+                    }
+                }*/
+
+                richTextBox1.Text = fileContent.ToString() + "\r\n" + "\r\n" + fileString + "\r\n" + 
+                    "\r\n" + words[0] + "\r\n" + "\r\n" +  words[words.Length-1] + "\r\n" + "\r\n" + 
+                    "The Longest word is " + longestWord + "\r\n" + "\r\n";
+
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "txt files (*.txt)|*.txt|Word docs (*.docx)|*.docx|Rich Text (*.rtf)|*.rtf";
+            saveDialog.Title = "Save a text file";
+            saveDialog.FilterIndex = 2;
+            saveDialog.RestoreDirectory = true;
+
+            if (saveDialog.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveDialog.OpenFile();
+            }
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveDialog.OpenFile()) != null) ;
+
+            }
+
+        }
+
+
+        /* 
+         * 
+         * 
+         *   Stream myStream;
+             SaveFileDialog saveDialog = new SaveFileDialog();
+             saveDialog.Filter = "txt files (*.txt)|*.txt|Word docs (*.docx)|*.docx|Rich Text (*.rtf)|*.rtf";
+             saveDialog.Title = "Save a text file";
+             saveDialog.FilterIndex = 2;
+             saveDialog.RestoreDirectory = true;
+
+             if (saveDialog.FileName != "")
+             {
+                 System.IO.FileStream fs = (System.IO.FileStream)saveDialog.OpenFile();
+             }
+             if (saveDialog.ShowDialog() == DialogResult.OK)
+             {
+                 if ((myStream = saveDialog.OpenFile()) != null);   
+             }
+ 
+         * OpenFileDialog fileDialog = new OpenFileDialog();
+         fileDialog.Filter = "txt files (*.txt)|*.txt|Word docs (*.docx)|*.docx|Rich Text (*.rtf)|*.rtf";
+
+             if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+             {
+                 filePath = fileDialog.FileName;
+
+                 var fileStream = fileDialog.OpenFile();
+
+         string[] delimiter = new string[] { " ", "\r\n", ",", ".", "(", ")" };
+                 using (StreamReader reader = new StreamReader(fileStream))
+                 {
+
+                     fileContent = reader.ReadToEnd();
+                 } */
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
