6c5eb1a8b0989549dac29b652743e779e68b0f5c
Tobias-1991 <62919937+Tobias-1991@users.noreply.github.com>
2020-08-28 14:49:49 -0700

    Add files via upload
    Dice Roller Visual Studio Program

---
+++ DiceRoller/DiceRoller/Dice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Threading;
+namespace DiceRoller
+{
+    // Create Dice class with 
+    // variables "sides" and "faceValue"
+    class Dice
+    {
+        private int sides;
+        private int faceValue;
+
+        public Dice(int sides)
+        {
+            this.sides = sides;
+            SidesValidation(sides);
+        }
+
+        // RollDie method returns the face value of rolled die
+        public int RollDie()
+        {
+            Random Random = new Random();
+
+            faceValue = Random.Next(1, sides);
+
+            return faceValue;
+        }
+
+        // Sides Validation method 
+        public void SidesValidation(int sides)
+        {
+            if (sides < 4 || sides > 20)
+            {
+                Console.WriteLine("You entered: " + sides + " Please enter number between 4 and 20.");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ DiceRoller/DiceRoller/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using System.Threading;
+namespace DiceRoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            int diceSides = 0;
+            int die1Roll = 0;
+            int die2Roll = 0;
+            int numRolls = 0;
+            bool snakeEyes = false;
+
+            try
+            {
+
+                Console.WriteLine("Welcome to Dice Roller!");
+                Console.WriteLine("Enter a positive integer between 4 and 20");
+                //take user input for number of dice sides
+                userInput = Console.ReadLine();
+                //convert to integer type
+                diceSides = Convert.ToInt32(userInput);
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            Dice die1 = new Dice(diceSides);
+            Dice die2 = new Dice(diceSides);
+            Console.WriteLine("Your " + diceSides + " sided dice are ready! Press any key to roll them");
+            Console.ReadKey(true);
+
+            do
+            {
+                die1Roll = die1.RollDie();
+                Console.WriteLine("Die 1 Rolled " + die1Roll);
+                die2Roll = die2.RollDie();
+                Console.WriteLine("Die 2 Rolled " + die2Roll);
+                numRolls++;
+                Console.WriteLine("You have rolled " + numRolls + " times.");
+
+                if (die1Roll == 1 && die2Roll == 1)
+                {
+                    snakeEyes = true;
+                    Console.WriteLine("Congrats! You rolled snake eyes in " + numRolls + " Rolls");
+                    Thread.Sleep(10000);
+                    Environment.Exit(0);
+                }
+            } while (snakeEyes == false);
+        }
+    }
+}
