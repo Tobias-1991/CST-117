e66a2fdfdad97d0039c66fcdf13b9e3746936d6b
Tobias-1991 <62919937+Tobias-1991@users.noreply.github.com>
2020-08-29 10:43:57 -0700

    Add files via upload
    Milestone 2 in progress

---
+++ Milestone2/Milestone2/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone2
+{
+    class Inventory
+    {
+        string itemName;
+        string itemSize;
+        int numbInStock;
+        int itemNumber;
+        double itemPrice;
+
+        public Inventory(string itemName, string itemSize, int numbInStock, int itemNumber, double itemPrice)
+        {
+            this.itemName = itemName;
+            this.itemSize = itemSize;
+            this.numbInStock = numbInStock;
+            this.itemNumber = itemNumber;
+            this.itemPrice = itemPrice;
+        }
+
+        public void Add()
+        {
+
+        }
+        public void Remove()
+        {
+
+        }
+        public void EditItem()
+        {
+
+        }
+        public void AddItem()
+        {
+
+        }
+        public void SortItems()
+        {
+
+        }
+        /*
+         What methods will be needed?
+         addItem
+         removeItem
+
+
+         */
+
+    } 
+}
