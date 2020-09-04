1e113321525e4c440d2e9c3332a32cb37ea525ad
Tobias-1991 <62919937+Tobias-1991@users.noreply.github.com>
2020-09-03 21:22:00 -0700

    CST-117 Exercise 8 Program File

--- Program.cs
+++ Program.cs
@@ -1,96 +1,72 @@
-﻿using Milestone2;
-using System;
-
+﻿using System;
 /*
- Zachary Gardner
+Zachary Gardner
 CST-117
-August 30, 2020
-This is my own work
+September 3, 2020
+Professor Pritchard
+
  */
 
-namespace MilestoneProject
+namespace Exercise8
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-            
-            //create inventory items in array
-            //I may use a list of arrays in the future.
-            Inventory[] Item = new Inventory[3];
-            Item[0] = new Inventory("Butterly Yoyo", 10, 14.99);
-            Item[1] = new Inventory("Looping Yoyo", 10, 10.99);
-            Item[2] = new Inventory("Kite", 10, 24.99);
+            //Declaring variables
+            int fatCals;
+            int carbCals;
+            //Welcome message
+            Console.WriteLine("Welcome to the Calorie Calculator");
 
-       
-            Console.WriteLine("Welcome to the Kite Shop!  Press any key to see inventory");
-            // Readkey pauses console until a key is pressed
-            Console.ReadKey();
-            // Clear removes all previous text from console
-            Console.Clear();
-            //reading and printing second object
-            for (int items = 0; items < Item.Length; items++)
-            {
-                Item[items].printInfo();
-            }
-            Console.WriteLine("Choose an option:");
-            Console.WriteLine("1) Purchase a Butterfly Yoyo");
-            Console.WriteLine("2) Purchase a Looping Yoyo");
-            Console.WriteLine("3) Purchase a Kite");
-            Console.WriteLine("4) Exit");
-            Console.Write("\r\nSelect an option: ");
+            //Console.ReadLine will take user input and save inside variables
+            Console.WriteLine("Please enter your total Carbohydrates for the day in grams");
+            string carbGrams = Console.ReadLine();
+
+            //Console.ReadLine will take user input and save inside variables
+            Console.WriteLine("Please enter your total Fats for the day in grams");
+            string fatGrams = Console.ReadLine();
+
+            //capturing data from methods
+            fatCals = FatCalories(Int32.Parse(fatGrams));
+            carbCals = CarbCalories(Int32.Parse(carbGrams));
+
+            //displaying the returned values
+            Console.WriteLine("Your total Calories from Carbs for the day are " + carbCals + 
+                "\r\nYour total Calories from Fat for the day are " + fatCals);
 
 
-            // Switch statement creates a great console menu
-            // Each case is established, user input decides which option will be run.
-            switch (Console.ReadLine())
+        }
+        //FatCalories method takes grams of fat as input, then multiplies by 9 to return number of calories from fat.
+        public static int FatCalories(int fatGrams)
+        {
+            int fatCals = 0;
+            fatCals = fatGrams * 9;
 
-            {
-                case "1":
-                    int totalPurchased = 0;
-                    Console.Clear();
-                    Console.WriteLine("How many would you like?");
-                    string val = Console.ReadLine();
-                    // string input must be converted to int datatype
-                    totalPurchased = Convert.ToInt32(val);
-                    // BuyItem method takes totalPurchased as parameter, and subtracts that number from 
-                    // the saved inventory amount
-                    Item[0].BuyItem(totalPurchased);
-                    // Simple for loop prints inventory items
-                    for (int items = 0; items < Item.Length; items++)
-                    {
-                        Item[items].printInfo();
-                    }
-                    return;
-                case "2":
-
-                    Console.Clear();
-                    Console.WriteLine("How many would you like?");
-                    val = Console.ReadLine();
-                    totalPurchased = Convert.ToInt32(val);
-                    Item[1].BuyItem(totalPurchased);
-                    for (int items = 0; items < Item.Length; items++)
-                    {
-                        Item[items].printInfo();
-                    }
-                    return;
-                case "3":
-                    Console.Clear();
-                    Console.WriteLine("How many would you like?");
-                    val = Console.ReadLine();
-                    totalPurchased = Convert.ToInt32(val);
-                    Item[2].BuyItem(totalPurchased);
-                    for (int items = 0; items < Item.Length; items++)
-                    {
-                        Item[items].printInfo();
-                    }
-                    return;
-                default:
-                  
-                    return;
-
-            }       
-        }    
+            return fatCals;
+        }
+        //CarbCalories method takes grams of Carbs as input, then multiplies by 4 to return number of calories from carbs.
+        public static int CarbCalories(int carbGrams)
+        {
+            int carbCals = 0;
+            carbCals = carbGrams * 4;
+            return carbCals;
+        }
     }
 }
+/*
+ * A nutritionist who works for a fitness club helps members by evaluating their diets. 
+ * As part of her evaluation, she asks members for the number of fat grams and carbohydrate grams that they consume in a day. 
+ * Then, she calculates the number of calories that result from the fat using the following formula: 
+ * Calories from fat = Fat grams × 9 
+ * Next, she calculates the number of calories that result from the carbohydrates using the following formula:
+ * Calories from carbs = Carbs grams × 4 
+ * Create an application that will make these calculations. 
+ * In the application, you should have the following methods:
+ * FatCalories–This method should accept a number of fat grams as an argument and return the number of calories from that amount of fat.
+ * CarbCalories–This method should accept a number of carbohydrate grams as an argument and return the number of calories from that amount of carbohydrates.
+ * 
+ * 
+ * 
+ * 
+ */
